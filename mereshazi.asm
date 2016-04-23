8e43a6ace46fdf98ba65ce9a6c7b7a271e2e5652
Mikitovics Márk <mikitovicsm@gmail.com>
2016-04-23 18:03:45 +0200

    button check with counting, next is pause

--- mereshazi.asm
+++ mereshazi.asm
@@ -44,6 +44,9 @@
 .def temp =r16 ; Temporary Register example 
 .def led =r17; LED Register
 .def dir =r18; Szamlalas irany Register
+.def btn =r19; 
+.def btnreg0 =r20; Shiftregiszter btn0-nak
+.def btnreg1 =r21; Shiftregiszter btn1-nek
 
 ;*************************************************************** 
 ;* Reset & Interrupt Vectors  
@@ -116,15 +119,43 @@ RESET:
 
 M_INIT:
 ;< ki- és bemenetek inicializálása stb > 
-ldi dir, increment;
+ldi dir, increment; alapbol felfele
+
 ldi temp, 0xFF ;
 out DDRC, temp ; Kimenet inicializálása
 ldi led, 0b1 ; kezdésnek az elsõ led világít
 out PORTC, led ; kiadjuk a kimenetre
 
+ldi temp, 0x00 ; 
+out DDRE, temp ; gombok bemenetre állítása
+sts DDRG, temp ; kapcsolók bemenetre állítása
+
+
 ;*************************************************************** 
 ;* MAIN program, Endless loop part
 M_LOOP:
+
+	call BUTTON_UPDATE;
+	jmp BTN_1_CHK;
+
+BTN_1_CHK:
+	sbrc btnreg1, 0 ; ha a gomb regiszter elsõ bitjének értéke 0, akkor kihagyjuk a jmp loopot
+	jmp BTN_0_CHK;
+	sbrs btnreg1, 1 ; ha a gomb regiszter második bitjének értéke 1, akkor kihagyjuk a jmp loopot
+	jmp BTN_0_CHK;
+	jmp BTN_1;
+	ldi btnreg1, 0; ne érzékelje úgy, hogy le van nyomva
+
+BTN_0_CHK:
+	sbrc btnreg0, 0 ;
+	jmp M_LOOP;
+	sbrs btnreg0, 1 ;
+	jmp M_LOOP;
+	jmp BTN_0;
+	ldi btnreg0, 0; ne érzékelje úgy, hogy le van nyomva
+
+
+BTN_1:
 	ldi temp, 0b1000;
 	sub temp, led;
 	breq LOAD_SEVEN;
@@ -141,17 +172,50 @@ M_LOOP:
 	sbrs temp, 1;
 	call STEP_INC;
 
-	jmp M_LOOP;
+	jmp BTN_1;
+
+BTN_0:
+	ldi temp, 0b1;
+	sub temp, led;
+	breq LOAD_FOUR;
+
+	ldi temp, 0b10000000;
+	sub temp, led;
+	breq LOAD_THREE;
+
+	mov temp, dir;;
+	sbrs temp, 0;
+	call STEP_DEC;
+
+	mov temp, dir;
+	sbrs temp, 1;
+	call STEP_INC;
+
+	jmp BTN_0;
+
 LOAD_SEVEN:
 	ldi dir, decrement;
 	ldi led, 0b10000000;
 	out PORTC, led ; kiadjuk a led értékét PORTC-n
-	jmp M_LOOP;
+	jmp BTN_1;
+
 LOAD_ZERO:
 	ldi dir, increment;
 	ldi led, 0b1;
 	out PORTC, led ; kiadjuk a led értékét PORTC-n
-	jmp M_LOOP;
+	jmp BTN_1;
+
+LOAD_THREE:
+	ldi dir, decrement;
+	ldi led, 0b1000;
+	out PORTC, led ; kiadjuk a led értékét PORTC-n
+	jmp BTN_0;
+
+LOAD_FOUR:
+	ldi dir, increment;
+	ldi led, 0b10000;
+	out PORTC, led ; kiadjuk a led értékét PORTC-n
+	jmp BTN_0;
 
 ;*************************************************************** 
 ;* Subroutines, Interrupt routines
@@ -174,6 +238,21 @@ RESET_LED:
 	ldi led, 0b1 ; visszaállítjuk 1re a led értékét
 	out PORTC, led ; kiadjuk itt is, mert a ret a main loopba fog visszatérni
 	ret
+
+BUTTON_UPDATE:
+	in btn, PINE
+	bst btn, 5 ; T-be töltjük btn 5. bitjét
+	lsl btnreg0 ; balra shifteljük az eddigi értékét a gomb regiszterünknek
+	bld btnreg0, 0 ; betöltjük T értékét a gomb regiszterünk elsõ helyiértékére
+	andi btnreg0, 0b11 ; maszkoljuk, csak az elsõ 2 bit érdekel minket
+
+	bst btn, 6 ; T-be töltjük btn 6. bitjét
+	lsl btnreg1 ;
+	bld btnreg1, 0 ;
+	andi btnreg1, 0b11 ;
+	
+	ret 
+
 
 
 
